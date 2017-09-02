4f5e5127b78608b96a62c826de199c9e575b2501
Tanapon Paetong <tanapon.paetong@gmail.com>
2017-09-02 15:35:02 +0700

    test playerA get first score Fifteen - Love

--- tennis-game.spec.js
+++ tennis-game.spec.js
@@ -1,19 +1,41 @@
-function TennisGame(){
-  this.reset = () =>{
+function TennisGame() {
+    const scoreString = ['Love', 'Fifteen', 'Thirty', 'Forthy']
+    this.playerAScore = 0
 
-  }
-  this.echo = () =>{
-    return 'LOVE - LOVE'
-  }
+    this.reset = () => {
+        this.playerAScore = 0
+    }
+
+    this.echo = () => {
+       return `${scoreString[this.playerAScore]} - Love`
+    }
+
+    this.playerAGteScore = () => {
+        this.playerAScore++
+    }
 }
 
-test('Echo "LOVE  - LOVE" when press reset button',() =>{
-  //Arrage
-  let app = new TennisGame()
-  //Act
-  app.reset();
-  let result = app.echo()
+test('Echo "Love - Love" when game start', () => {
+    // Arrange
+    let app = new TennisGame()
 
-  //Assert
-  expect(result).toBe('LOVE - LOVE')
+    // Act
+    app.reset()
+    let result = app.echo()
+
+    // Assert
+    expect(result).toBe('Love - Love')
+})
+
+test('Echo "Fifteen - Love" when playerA get first score', () => {
+    // Arrange
+    let app = new TennisGame()
+    app.reset()
+    app.playerAGteScore()
+
+    // Act
+    let result = app.echo()
+
+    // Assert
+    expect(result).toBe('Fifteen - Love')
 })
